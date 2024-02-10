a7d6ae29014a7debbdec541cde176eaeb900983a
murilo.badoco <guerreiro_murilo@hotmail.com>
2024-02-10 18:48:40 -0300

    create: add Async Storage and Persistence

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,16 +1,22 @@
-import { Header } from "@/components/header";
-import { Alert, ScrollView, Text, View } from "react-native";
-import { Product } from "@/components/product";
-import { ProductCartProps, useCartStore } from "./stores/cart-store";
-import { formatCurrency } from "@/utils/functions/format-currency";
-import { Input } from "@/components/input";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Button } from "@/components/button";
-import { Feather } from "@expo/vector-icons";
+import { Header } from "@/components/header";
+import { Input } from "@/components/input";
 import { LinkButton } from "@/components/link-button";
+import { Product } from "@/components/product";
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
+import { Alert, ScrollView, Text, View, Linking } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { ProductCartProps, useCartStore } from "./stores/cart-store";
+import { useNavigation } from "expo-router";
+
+const phoneNumber = process.env.EXPO_PUBLIC_PHONE_NUMBER;
 
 export default function Cart() {
+    const [address, setAddress] = useState("")
     const cartStore = useCartStore()
+    const navigation = useNavigation()
 
     const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.quantity * product.price, 0))
 
@@ -26,6 +32,31 @@ export default function Cart() {
         ])
     }
 
+    function handleOrder() {
+        if (address.trim().length === 0) {
+            return Alert.alert("Pedido", "Informe o endereço de entrega.")
+        }
+
+        const products = cartStore.products
+            .map((product) => `\n ${product.quantity} ${product.title}`)
+            .join("")
+
+        const message = `
+        🍔 NOVO PEDIDO
+        \n Entregar em: ${address}
+
+        ${products}
+
+        \n Valor total: ${total}
+        `
+        
+        Linking.openURL(`http://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`)
+
+        cartStore.clear()
+        navigation.goBack()
+
+    }
+
     return (
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho" />
@@ -36,7 +67,7 @@ export default function Cart() {
                         {cartStore.products.length > 0 ? (
                             <View className="border-b border-slate-700">
                                 {cartStore.products.map((product) => (
-                                    <Product key={product.id} data={product} onPress={() => handleProductRemove(product)}/>
+                                    <Product key={product.id} data={product} onPress={() => handleProductRemove(product)} />
                                 ))}
                             </View>
                         ) : (
@@ -48,13 +79,19 @@ export default function Cart() {
                             <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
                         </View>
 
-                        <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+                        <Input
+                            placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."
+                            onChangeText={setAddress}
+                            blurOnSubmit={true}
+                            onSubmitEditing={handleOrder}
+                            returnKeyType="next"
+                        />
                     </View>
                 </ScrollView>
             </KeyboardAwareScrollView>
 
             <View className="p-5 gap-5">
-                <Button>
+                <Button onPress={handleOrder}>
                     <Button.Text>Enviar Pedido</Button.Text>
                     <Button.Icon>
                         <Feather name="arrow-right-circle" size={20}></Feather>
--- src/app/stores/cart-store.ts
+++ src/app/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware"
 import * as cartInMemory from "./helpers/cart-in-memory"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export type ProductCartProps = ProductProps & {
     quantity: number
@@ -10,19 +12,29 @@ type StateProps = {
     products: ProductCartProps[]
     add: (product: ProductProps) => void
     remove: (productId: string) => void
+    clear: () => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-    products: [],
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
+        products: [],
 
-    add: (product: ProductProps) =>
-        set((state) => ({
-            products: cartInMemory.add(state.products, product),
-        })),
+        add: (product: ProductProps) =>
+            set((state) => ({
+                products: cartInMemory.add(state.products, product),
+            })),
 
-    remove: (productId: string) =>
-        set((state) => ({
-            products: cartInMemory.remove(state.products, productId),
-        })),
+        remove: (productId: string) =>
+            set((state) => ({
+                products: cartInMemory.remove(state.products, productId),
+            })),
 
-}))
+        clear: () =>
+            set((state) => ({
+                products: []
+            }))
+
+    }), {
+        name: "nlw-expert-rn:cart",
+        storage: createJSONStorage(() => AsyncStorage)
+    }))

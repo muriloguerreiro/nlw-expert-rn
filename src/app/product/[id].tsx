6367883962b8042b7933491843a80d25e4e375b3
murilo.badoco <guerreiro_murilo@hotmail.com>
2024-02-10 16:27:29 -0300

    create: add Cart Page and Remove Product feature

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/button";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
 import { Image, Text, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/link-button";
@@ -12,17 +12,24 @@ export default function Product() {
     const navigation = useNavigation()
     const { id } = useLocalSearchParams()
 
-    const product = PRODUCTS.filter((item) => item.id === id)[0]
+    const product = PRODUCTS.find((item) => item.id === id)
 
-    function handleAddToCart(){
-        cartStore.add(product)
-        navigation.goBack()
+    function handleAddToCart() {
+        if (product) {
+            cartStore.add(product)
+            navigation.goBack()
+        }
+    }
+
+    if (!product) {
+        return <Redirect href="/" />
     }
 
     return (
         <View className="flex-1">
-            <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
+            <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
             <View className="p-5 mt-8 flex-1">
+                <Text className="text-white text-xl font-heading">{product.title}</Text>
                 <Text className="text-lime-400 text-2xl font-heading my-2">{formatCurrency(product.price)}</Text>
                 <Text className="text-slate-400 font-body text-base leading-6 mb-6">{product.description}</Text>
                 {product.ingredients.map((ingredient) => (
@@ -30,13 +37,13 @@ export default function Product() {
                 ))}
             </View>
             <View className="p-5 pb-8 gap-5">
-                <Button onPress={handleAddToCart}>    
+                <Button onPress={handleAddToCart}>
                     <Button.Icon>
                         <Feather name="plus-circle" size={20} />
                     </Button.Icon>
                     <Button.Text>Adicionar ao pedido</Button.Text>
                 </Button>
-                <LinkButton title="Voltar ao cardápio" href="/"/>
+                <LinkButton title="Voltar ao cardápio" href="/" />
             </View>
         </View>
     )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Feather } from "@expo/vector-icons"
+import { Link } from "expo-router"
 import colors from "tailwindcss/colors";
 
 type HeaderProps = {
@@ -14,13 +15,16 @@ export function Header({ title, cartQuantityItems = 0 }: HeaderProps) {
                 <Image source={require("@/assets/logo.png")} className="h-6 w-32" />
                 <Text className="text-white text-xl font-heading mt-2">{title}</Text>
             </View>
-            { cartQuantityItems > 0 &&
-                <TouchableOpacity className="relative" activeOpacity={0.6}>
-                <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
-                    <Text className="text-slate-900 font-bold text-xs">{cartQuantityItems}</Text>
-                </View>
-                <Feather name="shopping-bag" color={colors.white} size={24} />
-            </TouchableOpacity>}
+            {cartQuantityItems > 0 &&
+                <Link href="/cart" asChild>
+                    <TouchableOpacity className="relative" activeOpacity={0.6}>
+                        <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
+                            <Text className="text-slate-900 font-bold text-xs">{cartQuantityItems}</Text>
+                        </View>
+                        <Feather name="shopping-bag" color={colors.white} size={24} />
+                    </TouchableOpacity>
+                </Link>
+            }
         </View>
     )
 }
